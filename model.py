d6e8c508bde3047490a2aab54e55816a58696368
Matija Kocbek <mkocbek1@gmail.com>
2021-05-11 15:34:11 +0200

    Narejen .json

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 STEVILO_DOVOLJENIH_NAPAK = 10
 
@@ -10,11 +11,46 @@ ZACETEK = 'S'
 ZMAGA = 'W'
 PORAZ = 'L'
 
+DATOTEKA_ZA_SHRANJEVANJE = "podatki.json"
+
 class Vislice:
-    def __init__(self):
-        self.igre = {}
-        self.max_id = 0
+    def __init__(self, zacetne_igre=None, zacetni_id=0):
+        self.igre = zacetne_igre or {}
+        self.max_id = zacetni_id
     
+    def pretvor_v_json_slovar(self):
+        slovar_iger = {}
+
+        for id_igre, (igra,stanje) in self.igre.items():
+            slovar_iger[id_igre] = (
+                igra.pretvor_v_json_slovar(),
+                stanje
+            )
+
+        return {
+            "max_id": self.max_id,
+            "igre": slovar_iger
+        }
+
+    def zapisi_v_datoteko(self, datoteka):
+        with open(datoteka,"w") as out_file:
+            json_slovar = self.pretvor_v_json_slovar()
+            json.dump(json_slovar,out_file)    
+    @classmethod
+    def dobi_iz_json_slovarja(cls, slovar):
+        slovar_iger = {}
+        for id_igre, (igra_slovar,stanje) in slovar["igre"].items():
+            slovar_iger[int(id_igre)] = (
+                Igra.dobi_iz_json_slovarja(igra_slovar),stanje
+            )
+        return Vislice(slovar_iger,slovar["max_id"])
+
+    @staticmethod
+    def preberi_iz_datoteke(datoteka):
+        with open(datoteka,"r") as in_file:
+            json_slovar = json.load(in_file)
+        return Vislice.dobi_iz_json_slovarja(json_slovar)
+
     def prost_id_igre(self):
         self.max_id += 1
         return self.max_id
@@ -91,6 +127,15 @@ class Igra:
                     return PORAZ
                 else:
                     return NAPACNA_CRKA
+    def pretvor_v_json_slovar(self):
+        return {
+            "geslo": self.geslo,
+            "crke": self.crke
+        }
+
+    @staticmethod
+    def dobi_iz_json_slovarja(slovar):
+        return Igra(slovar["geslo"],slovar["crke"])
 
 with open('besede.txt',"r",encoding="utf-8") as f:
     bazen_besed = [vrstica.strip().upper() for vrstica in f]
--- vislice.py
+++ vislice.py
@@ -9,22 +9,26 @@ def index():
 
 @bottle.post("/igra/")
 def nova_igra():
+    vislice = model.Vislice.preberi_iz_datoteke(model.DATOTEKA_ZA_SHRANJEVANJE)
     id_igre = vislice.nova_igra()
     novi_url = f"/igra/{id_igre}/"
+    vislice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
     bottle.redirect(novi_url)
 
 @bottle.get("/igra/<id_igre:int>/")
 def pokazi_igro(id_igre):
+    vislice = model.Vislice.preberi_iz_datoteke(model.DATOTEKA_ZA_SHRANJEVANJE)
     trenutna_igra, trenutno_stanje = vislice.igre[id_igre]
-
+    vislice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
 
     return bottle.template("igra.tpl",stanje=trenutno_stanje,igra=trenutna_igra)
 
 @bottle.post("/igra/<id_igre:int>/")
 def ugibaj_na_igri(id_igre):
+    vislice = model.Vislice.preberi_iz_datoteke(model.DATOTEKA_ZA_SHRANJEVANJE)
     ugibana = bottle.request.forms.get("crka")
     vislice.ugibaj(id_igre,ugibana)
-
+    vislice.zapisi_v_datoteko(model.DATOTEKA_ZA_SHRANJEVANJE)
     return bottle.redirect(f"/igra/{id_igre}/")
 
 @bottle.route("/img/<file_path:path>")
